b4c25f93d58836c951072c9ffb8476ada5dcc951
WaleedAboHashima <waleedsabryyyy@gmail.com>
2023-06-01 14:01:38 +0300

    final

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -1,15 +1,4 @@
 const mongoose = require("mongoose");
 mongoose.set({ strictPopulate: false });
 
-module.exports = () =>
-  mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Increase timeout
-    socketTimeoutMS: 45000, // Increase timeout
-    family: 4, // Use IPv4, skip trying IPv6
-    autoIndex: false, // Don'tbuild indexes
-    keepAlive: true, // Keep socket connections alive
-    keepAliveInitialDelay: 300000, // Wait 5 minutes before starting keepAlive
-    connectTimeoutMS: 30000,
-  });
+module.exports = () => mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
