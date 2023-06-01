e727b9aabe3dd17d0f1b8d4ea867f02a310261e9
WaleedAboHashima <waleedsabryyyy@gmail.com>
2023-06-01 13:48:43 +0300

    Mongo Restrict

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 mongoose.set({ strictPopulate: false });
 
-module.exports = () => mongoose.connect(process.env.DATABASE_URL);
+module.exports = () => mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
