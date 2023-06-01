f1f1308b610c3dcf8ea4e747b0de670fca14019b
WaleedAboHashima <waleedsabryyyy@gmail.com>
2023-06-01 13:59:00 +0300

    Db change again

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -9,11 +9,7 @@ module.exports = () =>
     socketTimeoutMS: 45000, // Increase timeout
     family: 4, // Use IPv4, skip trying IPv6
     autoIndex: false, // Don'tbuild indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    bufferSize: 0, // Disable buffering
     keepAlive: true, // Keep socket connections alive
     keepAliveInitialDelay: 300000, // Wait 5 minutes before starting keepAlive
-    connectTimeoutMS: 30000, // Timeout for initial connection
-    retryAttempts: 10, // Retry up to 10 times
-    retryDelay: 1000,
+    connectTimeoutMS: 30000,
   });
