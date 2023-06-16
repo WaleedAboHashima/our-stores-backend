ab01f6663e992385448b4d94b0f96524abfd21e9
WaleedAboHashima <waleedsabryyyy@gmail.com>
2023-06-16 17:10:12 +0300

    undo

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,13 +35,12 @@ exports.GetStores = asyncHandler(async (req, res) => {
 });
 
 exports.GetStates = asyncHandler(async (req, res, next) => {
-  const { gove } = req.body;
+  const { gove } = req.query;
   const data = await Governments.findOne({ name: gove })
   if (data) {
     res.status(200).json(data);
   }
   else {
     res.status(200).json([])
-
   }
 });
